1091a5e3f342baa8ab27fbfb5e58933ecfe24f7f
0xturboblitz <florent.tavernier@gmail.com>
2022-08-15 23:12:00 +0200

    initial commit

--- tree.ts
+++ tree.ts
@@ -2,57 +2,73 @@ import { utils } from 'ethers'
 
 class MerkleTree {
     
-    hashes: string[] = []
+    levels: string[][] = []
+    index: number = 0
+    layers: number = 0
     
-    constructor(data: string[]) {
-        this.init(data)
+    constructor(layers: number) {
+        this.init(layers)
+        this.layers = layers
     }
     
-    init(data: string[]) {
-        this.hashes = data.map(e => utils.keccak256(utils.toUtf8Bytes(e)))
-    
-        let length = this.hashes.length
-        let offset = 0
-    
-        while (length > 1) {
-            if (length % 2 != 0) {
-                this.hashes.push(this.hashes[this.hashes.length - 1])
-                length++
-            }
-            for (let i = 0; i < length; i += 2) {
-                const array =  Uint8Array.from([...utils.arrayify(this.hashes[offset + i]), ...utils.arrayify(this.hashes[offset + i + 1])])
-                this.hashes.push(utils.keccak256(array))
-            }
-            offset += length
-            length /= 2
+    init(layers: number) {
+        // fill tree with zeros
+        for (let i = 0; i <= layers; i++) {
+            this.levels[i] = new Array(2**(layers - i)).fill(this.zeros(i))   
         }
-    }
-
-    verify(leaf: string, proof: string[]) {
-        let hash = utils.keccak256(utils.toUtf8Bytes(leaf))
-    
-        for (let i = 0; i < proof.length; i++) {
-            hash = utils.keccak256(Uint8Array.from([...utils.arrayify(hash), ...utils.arrayify(proof[i])]))
-        }
-    
-        return hash === this.getRoot()
     }
 
     getRoot() {
-        return this.hashes[this.hashes.length - 1]
+        return this.levels[0][0]
     }
+
+    hash(a: string, b: string) {
+        return utils.keccak256(a + b.slice(2))
+    }
+
+    zeros(i: number) {
+        if (i === 0) return "0x2fe54c60d3acabf3343a35b6eba15db4821b340f76e741e2249685ed4899af6c"
+        else if (i === 1) return "0x256a6135777eee2fd26f54b8b7037a25439d5235caee224154186d2b8a52e31d"
+        else if (i === 2) return "0x1151949895e82ab19924de92c40a3d6f7bcb60d92b00504b8199613683f0c200"
+        else if (i === 3) return "0x20121ee811489ff8d61f09fb89e313f14959a0f28bb428a20dba6b0b068b3bdb"
+        else if (i === 4) return "0x0a89ca6ffa14cc462cfedb842c30ed221a50a3d6bf022a6a57dc82ab24c157c9"
+        else if (i === 5) return "0x24ca05c2b5cd42e890d6be94c68d0689f4f21c9cec9c0f13fe41d566dfb54959"
+        else if (i === 6) return "0x1ccb97c932565a92c60156bdba2d08f3bf1377464e025cee765679e604a7315c"
+        else if (i === 7) return "0x19156fbd7d1a8bf5cba8909367de1b624534ebab4f0f79e003bccdd1b182bdb4"
+    }
+
+    addLeaf(leaf: string) {
+        this.levels[0][this.index] = leaf
+        let idx = this.index
+
+        for (let i = 0; i < this.layers; i++) {
+            const thisLevel = this.levels[i]
+            if (idx % 2 === 0) {
+                this.levels[i + 1][idx/2] = this.hash(thisLevel[idx], thisLevel[idx + 1])
+            } else {
+                this.levels[i + 1][(idx - 1)/2] = this.hash(thisLevel[idx - 1], thisLevel[idx])
+                idx--
+            }
+            idx /= 2
+        }
+
+        this.index++
+    }
+
+    //verify
+    //forking: addLeaf(leaf: string, lastHonnestIndex: number)
 }
 
 
 function example() {
-    let tree = new MerkleTree(["salut", "toi", "c'est", "moi", "et"])
+    let tree = new MerkleTree(3)    
+    console.log('tree.levels :', tree.levels)
 
-    console.log('tree.hashes :', tree.hashes)
-    console.log(tree.verify("salut",
-    ["0xa0057ef29cc215d2dbacc6c77ce4b2fdf282d5dd0ae313de4b55a5163957707e",
-    "0x340135d068356183ba2c1d760a4781fcb4bf2018d0d7a398e5499c16b201ed8c",
-    "0x589669b0b47fc86d69d388081745420197ef7d306defde6932e1b54206507619"
-    ]))
+    tree.addLeaf("0x0a89ca6ffa14cc462cfedb842c30ed221a50a3d6bf022a6a57dc82ab24c157c9")
+    tree.addLeaf("0x24ca05c2b5cd42e890d6be94c68d0689f4f21c9cec9c0f13fe41d566dfb54959")
+    tree.addLeaf("0x1ccb97c932565a92c60156bdba2d08f3bf1377464e025cee765679e604a7315c")
+    tree.addLeaf("0x19156fbd7d1a8bf5cba8909367de1b624534ebab4f0f79e003bccdd1b182bdb4")
+    console.log('tree.levels :', tree.levels)
 }
 
 example()
